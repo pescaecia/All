4230761eb19ca9b2c69fb37aef4f9c5cd5faa37f
vinicius <viniciusnazario25@gmail.com>
2019-04-12 20:44:11 -0300

    dadada

--- Sql/Databaserson.sql
+++ Sql/Databaserson.sql
@@ -1,3 +1,6 @@
-create table jorje();
-
-select * from jorje();
+create table usuario(
+id int primary key auto_increment,
+nome varchar (50) NOT NULL,
+login varchar(20) NOT NULL,
+senha varchar(20) NOT NULL
+);
