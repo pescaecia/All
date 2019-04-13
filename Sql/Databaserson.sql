ec1e4134b753196fa2176a5313f4a6be68f1e5ef
HP <HP@HP-PC>
2019-04-12 21:24:29 -0300

    Lula rato, hitler stalin and mussolini in my heart. vinicio gado etc

--- Sql/Databaserson.sql
+++ Sql/Databaserson.sql
@@ -6,13 +6,13 @@ senha varchar(20) NOT NULL,
 email varchar(30) NOT NULL Unique
 );
 
-create table puclicacao(usuario_id int, foreign key (id) references usuario(id),
+create table publicacao(usuario_id int, foreign key (id) references usuario(id),
 id int primary key auto_increment,
 titulo varchar(50) NOT NULL,
-conteudo varchar(250) NOT NULL, 
+conteudo varchar(250) NOT NULL
 );
 
-create table mensagens(id int primary auto_increment,
+create table mensagens(id int primary key auto_increment,
 id_session int, foreign key (id) references usuario(id),
 id_recebedor int, foreign key (id) references usuario(id),
 conteudo varchar(200)
@@ -32,7 +32,7 @@ senha varchar(30)
 create table publicacaoAdm(adm_id int, foreign key (id) references administrador(id),
 id int primary key auto_increment,
 titulo varchar(50) NOT NULL,
-conteudo varchar(250) NOT NULL, 
+conteudo varchar(250) NOT NULL
 );
 
 
