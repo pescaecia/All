f0c5de82449c7764638cf6b7743963439a97ad38
vinicius <viniciusnazario25@gmail.com>
2019-04-16 22:10:58 -0300

    DDSADSADSAS

--- Cadastro/Cadastrar.php
+++ Cadastro/Cadastrar.php
@@ -1,6 +1,12 @@
 <html>
     <head>
-        <title>Cadastramento Etc</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+        <title>Pesca inicial</title>
+        <link rel="stylesheet" type="text/css" href="EstiloEtc.css">
+
+    </head>
     </head>
     <body>
         <form method="post" action="#" id="cadastro">
