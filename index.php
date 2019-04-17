82a5fdf3dd9e978ad45155fd42b62ce314a972b0
HP <HP@HP-PC>
2019-04-16 21:21:18 -0300

    Lula rato, hitler stalin and mussolini in my heart. vinicio gado etc

--- index.php
+++ index.php
@@ -17,40 +17,14 @@
             </div>
 
              <ul class="list-unstyled components">
-        
-                <li class="">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
-                    <ul class="collapse list-unstyled" id="homeSubmenu">
-                        <li>
-                            <a href="Cadastro.php">cadastro1</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 3</a>
-                        </li>
-                    </ul>
+                <li>
+                    <a href="#">Forum</a>
                 </li>
                 <li>
-                    <a href="#">About</a>
+                    <a href="#pageSubmenu">Amizades Virtuais</a>
                 </li>
                 <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
+                    <a href="#">E dando dicas</a>
                 </li>
                 <li>
                     <a href="#">Contact</a>
@@ -58,9 +32,7 @@
             </ul>
 
         </nav>
-
-        <!-- Page Content  -->
-        <div id="content">
+      <div id="content">
 
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
