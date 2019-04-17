4df0cac1a57b16a4227ba111a88ac17c5bd467bf
HP <HP@HP-PC>
2019-04-16 22:03:30 -0300

    Lula rato, hitler stalin and mussolini in my heart. vinicio gado etc

--- Cadastrar.php
+++ Cadastrar.php
@@ -1,5 +1,16 @@
 <html>
-    
+    <head>
+        <title>Cadastramento Etc</title>
+    </head>
+    <body>
+        <form method="post" action="#" id="cadastro">
+            <p> Nome:  <input id="nome" type="text" name="nome"> </p>
+            <p> Login: <input id="login" type="text" name="login"> </p>
+            <p> Senha: <input id="senha" type="text" name="senha"> </p>
+            <p> E-mail: <input id="email" type="text" name="email"> </p>
+            <input type="submit" id="submiter" value="Cadastrar" name="cadastrar">
+        </form>
+    </body>
 </html>
 <?php
 
