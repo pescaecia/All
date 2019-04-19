ba2b61ae6bf9c840d3aca63fc9fdbc8cc4f9c117
HP <HP@HP-PC>
2019-04-18 21:54:38 -0300

    Lula rato, hitler stalin and mussolini in my heart. vinicio gado etc

--- Login/Autenticacao.php
+++ Login/Autenticacao.php
@@ -1,2 +1,45 @@
 <?php
 
+session_start();
+
+function logar ($login, $senha){
+    $_SESSION["login"] = $login;
+    $_SESSION["senha"] = $senha;
+    iniciarTempoSessao();
+}
+
+function deslogar(){
+    session_destroy();
+}
+
+function sessãoExpirada(){
+    if ($_SESSION["tempo"] < time()) {
+        return true;
+    } else {
+        // reiniciar sessao
+        iniciarTempoSessao();
+        return false;
+    }
+}
+
+function autenticar() {
+    //se NAO estaLogado ou sessaoExpirada
+    if (!estaLogado() || sessaoExpirada()) {
+        deslogar();
+        header('Location: ../index.php');
+    } else {
+        return true;
+    }
+}
+
+function estaLogado() {
+    return isset($_SESSION['login']);
+}
+
+function exibirUsername() {
+    return $_SESSION['login'];
+}
+
+function iniciarTempoSessao(){
+    $_SESSION["tempo"] = time();
+}
