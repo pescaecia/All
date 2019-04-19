bd6e03c9dc2c002df9aa95dd28ff7e9616484f1b
HP <HP@HP-PC>
2019-04-18 23:34:37 -0300

    Lula rato, hitler stalin and mussolini in my heart. vinicio gado etc

--- Login/Logar.php
+++ Login/Logar.php
@@ -1,11 +1,12 @@
 <?php
 include './Autenticacao.php';
 include '../Sql/Conexao.php';
-
 ini_set("display_errors", true);
 
 $username = $_POST['login'];
 $password = $_POST['senha'];
+
+
 function getLogin($conector,$sql){
     $retorno = mysqli_query($conector,$sql);
     $resultado = mysqli_fetch_array($retorno);
@@ -13,16 +14,26 @@ function getLogin($conector,$sql){
 }
  
 $result = getLogin($conector, "select * from administrador where login = '$username' and senha = '$password'");
-echo $result;
 if($result != Null){
     logar($result['username'], $result['id'], "A");
     header('Location: ../Redirecionar.php');
     die();
 }
 $result = getLogin($conector, "select * from usuario where login = '$username' and senha = '$password'");
-echo $result;
 if($result != Null){
     logar($result['username'], $result['id'], "U");
     header('Location: ../Redirecionar.php');
     die();
 }
+  $result = getLogin($conector, "select * from administrador where login = '$username' and senha = '$password'");
+    if ($result == Null) {
+      
+    echo("<script type='text/javascript'> alert('Usuario e/ou senha incorretos !!!'
+            ); location.href='../index.php';</script>");
+    }
+      $result = getLogin($conector, "select * from usuario where login = '$username' and senha = '$password'");
+    if ($result == Null) {
+      
+    echo("<script type='text/javascript'> alert('Usuario e/ou senha incorretos !!!'
+            ); location.href='../index.php';</script>");
+    }
